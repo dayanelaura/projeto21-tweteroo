3373452d15a2d8b1b2caf9dfd12c54ecb4e930bb
Dayane <dayanelaura3@gmail.com>
2023-08-25 03:38:57 -0300

    feat: POST /sign-up route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,13 @@
 import {
+  Body,
   Controller,
   Get,
+  HttpCode,
+  HttpStatus,
+  Post,
 } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoginDTO } from './dtos/login.dto';
 
 @Controller()
 export class AppController {
@@ -11,5 +16,11 @@ export class AppController {
   @Get()
   getHealth(): string {
     return this.appService.getHealth();
+  }
+
+  @Post("sign-up")
+  @HttpCode(HttpStatus.OK)
+  signUp(@Body() body: LoginDTO){
+    return this.appService.login(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AppService {
+  private users: User[];
+
+  constructor(){
+    this.users = [];
+  }
+
   getHealth(): string {
     return "I'm okay!";
+  }
+
+  login(body: any){
+    const {username, avatar} = body;
+    return this.users.push(new User(username, avatar));
   }
 }
